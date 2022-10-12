e16d5534528d5d11ac4f4bcd9674616f642f297d
dahabwallet <114296740+dahabwallet@users.noreply.github.com>
2022-10-12 14:14:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import { changeColor } from './main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={changeColor}>
-          Submit
-        </button>
+        <p>Hello World</p>
       </header>
     </div>
   );
