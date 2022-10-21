5dd054a5f99d7def110441da8b0c97967ab1a4b6
Omar Diaa <omardiaa500@gmail.com>
2022-10-21 22:20:20 +0200

    Created create_wallet function

---
+++ src/scripts/create_wallet.js
@@ -0,0 +1,9 @@
+import store_keypair from "./store_keypair";
+import { ethers } from "ethers"
+
+const create_wallet = () => {
+	const key_pair = ethers.Wallet.createRandom()._signingKey();
+	store_keypair(key_pair.publicKey, key_pair.privateKey)
+}
+
+export default create_wallet
