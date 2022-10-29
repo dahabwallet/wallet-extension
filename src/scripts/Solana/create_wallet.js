a82e04326a9e1056a869772bcb09cfdfdab9f9d1
Omar Diaa <omardiaa500@gmail.com>
2022-10-29 23:06:46 +0200

    Created create_wallet

---
+++ src/scripts/Solana/create_wallet.js
@@ -0,0 +1,12 @@
+import store_keypair from "../store_keypair";
+import * as solanaWeb3 from '@solana/web3.js';
+
+const create_wallet = () => {
+	let keypair = solanaWeb3.Keypair.generate();
+	let privateKey = keypair.secretKey.toString();
+	let publicKey = keypair.publicKey.toString()
+
+	store_keypair("SOL", publicKey, privateKey)
+}
+
+export default create_wallet
