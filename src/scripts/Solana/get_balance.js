6586053bd6d77b6e57ef1e3b74b547028296dfa8
Omar Diaa <omardiaa500@gmail.com>
2022-10-30 22:53:28 +0200

    Created get_balance function

---
+++ src/scripts/Solana/get_balance.js
@@ -0,0 +1,12 @@
+
+import { PublicKey, Connection, clusterApiUrl } from '@solana/web3.js';
+
+const get_balance = async (publicKey) => {
+  let connection = new Connection(clusterApiUrl("testnet"));
+
+  publicKey = new PublicKey(publicKey);
+  let balance = await connection.getBalance(publicKey);
+  return balance
+}
+
+export default get_balance
