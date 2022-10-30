297c8185ae5e6a4903432a56e97da30443688a70
Omar Diaa <omardiaa500@gmail.com>
2022-10-30 23:42:52 +0200

    Created make_transfer_transaction on solana

---
+++ src/scripts/Solana/make_transfer_transaction.js
@@ -0,0 +1,31 @@
+
+import * as buffer from "buffer";
+import {
+  Keypair, Transaction, SystemProgram, LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction, Connection, clusterApiUrl, PublicKey
+} from '@solana/web3.js';
+
+const send_transaction = async (sender_priv_key, receiver_pub_key, amount) => {
+  window.Buffer = buffer.Buffer;
+
+  const connection = new Connection(clusterApiUrl("testnet"));
+  const toPublicKey = new PublicKey(receiver_pub_key);
+
+  sender_priv_key = sender_priv_key.split(',').map(str => Number(str))
+  let fromKeypair = Keypair.fromSecretKey(Uint8Array.from(sender_priv_key));
+
+  let transaction = new Transaction();
+  transaction.add(
+    SystemProgram.transfer({
+      fromPubkey: fromKeypair.publicKey,
+      toPubkey: toPublicKey,
+      lamports: LAMPORTS_PER_SOL * amount,
+    }),
+  );
+
+  await sendAndConfirmTransaction(connection, transaction, [fromKeypair])
+}
+
+export default send_transaction
+
+
