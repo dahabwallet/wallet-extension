5d0d2b66af4b16a7cf79c1da7d63b9808ef38455
shinawy <mahmoudelshenawy@aucegypt.edu>
2022-11-03 01:05:08 +0200

    testing gitignore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MakeTransaction from "./pages/MakeTransaction/MakeTransaction"
 import CreateWallet from "./pages/CreateWallet/CreateWallet";
 import privatekey_exists from "./scripts/privatekey_exists"
 import TransactionSuccessful from "./pages/MakeTransaction/TransactionSuccessful";
-
+import Report from "./pages/Report/Report";
 
 // import privatekey_exists from "../../scripts/privatekey_exists"
 
