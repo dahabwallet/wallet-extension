bae92c51231ea970fb2dfc8ff52db39e583179ff
shinawy <mahmoudelshenawy@aucegypt.edu>
2022-11-04 17:35:45 +0200

    updated the eth_get_balance function to disregard the private key as a parameter - In the following phases I will remove it from the signature and update the function calls accordingly

--- src/scripts/ethereum/eth_get_balance.js
+++ src/scripts/ethereum/eth_get_balance.js
@@ -1,11 +1,12 @@
 import { ethers } from "ethers"
 
 async function ethGetBalance(sender_priv_key) {
-    let provider = new ethers.providers.getDefaultProvider("goerli")
-    let walletPrivKey = new ethers.Wallet(sender_priv_key)
 
-    const wallet = walletPrivKey.connect(provider)
-    let wallet_balance = await wallet.getBalance()
+    let pub_key= window.localStorage.getItem('ETH_publicKey')
+    let provider = new ethers.providers.getDefaultProvider("goerli")
+    let wallet_balance= await provider.getBalance(pub_key)
+
+
     wallet_balance = ethers.utils.formatEther(wallet_balance)
 
     return wallet_balance
