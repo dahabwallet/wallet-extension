42e7cc1347c59c9fbff0a48f98b2c37ce667f6f5
Omar Diaa <omardiaa500@gmail.com>
2022-11-16 07:15:56 +0200

    Polygon working

--- src/scripts/Polygon/get_balance.js
+++ src/scripts/Polygon/get_balance.js
@@ -3,9 +3,9 @@ import { ethers } from "ethers"
 async function getBalance(sender_priv_key) {
 
   const dic_net = {
-    name: 'matic',
+    name: 'Mumbai Testnet',
     chainId: 80001,
-    _defaultProvider: (providers) => new providers.JsonRpcProvider('https://rpc-mumbai.matic.today/')
+    _defaultProvider: (providers) => new providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com/')
   };
 
   let provider = new ethers.providers.getDefaultProvider(dic_net)
