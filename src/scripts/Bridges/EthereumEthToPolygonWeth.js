7ba7ab56d65ee41592ceb976f73dd99d00b83187
Omar Diaa <omardiaa500@gmail.com>
2022-11-30 10:14:47 +0200

    Added get_balance weth on polygon + file structure organized

--- src/scripts/Bridges/EthereumEthToPolygonWeth.js
+++ src/scripts/Bridges/EthereumEthToPolygonWeth.js
@@ -1,6 +1,6 @@
 import { transferETHFromEthereumToMaticUsingPOSBridge } from "@ethereumnetwork/matic-bridge";
 
-const ethereumEthToPolygonWeth = (sender_priv_key, receiver_pub_key, amount) => {
+const ethereumEthToPolygonWeth = async (sender_priv_key, receiver_pub_key, amount) => {
   await transferETHFromEthereumToMaticUsingPOSBridge({
     maticApiUrl: 'https://rpc-mumbai.maticvigil.com',
     ethereumApiUrl: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
