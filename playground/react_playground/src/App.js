03d49ab1b8758dad9c892b450e9a7edd92e685a7
shinawy <mahmoudelshenawy@aucegypt.edu>
2023-03-01 01:52:58 +0200

    simplified the test playground react project to handle only one page to solve the routing error

--- playground/react_playground/src/App.js
+++ playground/react_playground/src/App.js
@@ -1,27 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TwoFishPage from "./Pages/TwoFishPage";
+import Mnemon from "./pages/Mnemonics";
 
 
 function App() {
   return (
-//     <Router>
-//     <Routes>
-      
-//       <Route path="/" element={<TwoFishPage />} />
+    
+    <Router>
+      <Routes>
+        
+        <Route path="/" element={<Mnemon />} />
 
-//     </Routes>
-// </Router>
+      </Routes>
+    </Router>
 
-<div>
-    <p>hello from the other side</p>
-    <script src="node_modules/argon2-browser/dist/argon2-bundled.min.js"> 
-    </script>
-    <script>
-    {console.log("argon2: ", argon2)}
-    </script> 
 
-  </div>
+
+
     
     );
   
---
+++ playground/react_playground/src/pages/Mnemonics.js
@@ -0,0 +1,46 @@
+
+
+// import colors from "../includes/colors"
+
+
+
+
+
+const  Mnemon =  () => {
+
+  return (
+   
+    <div>
+    <p>hello from the other side</p>
+
+    </div>
+
+  );
+}
+  
+
+// const styles = {
+//   parentStyle: {
+//     height: "100vh",
+//     width: "100vw",
+//     backgroundColor: colors['grey-background'],
+//     flexDirection: "column",
+//     "font-family": 'Montserrat Alternates',
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   },
+//   btnStyle: {
+//     fontWeight: 'bold',
+//     fontSize: 18,
+//     color: "white",
+//     backgroundColor: colors['orange'],
+//     border: "none",
+//     marginBottom: 20
+//   },
+//   imgStyle: {
+//     width: 240,
+//     height: 200
+//   }
+// }
+export default Mnemon;
