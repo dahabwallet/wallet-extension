96967c1a628a5d18f6bc272079e15b1c45cb5e67
omariosman <omarosman23@aucegypt.edu>
2023-05-22 23:31:15 +0300

    document create_wallet
    Signed-off-by: omariosman <omarosman23@aucegypt.edu>

--- src/scripts/Solana/create_wallet.js
+++ src/scripts/Solana/create_wallet.js
@@ -1,11 +1,24 @@
+// Importing the required modules
 import store_keypair from "../store_keypair";
 import * as solanaWeb3 from '@solana/web3.js';
 
+/**
+ * Generates a Solana wallet keypair and stores it.
+ * The generated keypair consists of a public key and a private key.
+ * The keypair is stored using the store_keypair function.
+ * @returns {void}
+ */
 const create_wallet = () => {
-	let keypair = solanaWeb3.Keypair.generate();
-	let privateKey = keypair.secretKey.toString();
-	let publicKey = keypair.publicKey.toString()
-	store_keypair("SOL", publicKey, privateKey);
+    // Generating a new keypair using the Solana Keypair class
+    let keypair = solanaWeb3.Keypair.generate();
+
+    // Converting the private and public keys to strings
+    let privateKey = keypair.secretKey.toString();
+    let publicKey = keypair.publicKey.toString();
+
+    // Storing the keypair using the store_keypair function
+    store_keypair("SOL", publicKey, privateKey);
 }
 
-export default create_wallet
+// Exporting the create_wallet function as the default export of the module
+export default create_wallet;
