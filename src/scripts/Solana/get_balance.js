1ddea565bcb3f3b00d2a03a51365c823cdfe56dc
omariosman <omarosman23@aucegypt.edu>
2023-05-22 23:32:11 +0300

    document get balance
    Signed-off-by: omariosman <omarosman23@aucegypt.edu>

--- src/scripts/Solana/get_balance.js
+++ src/scripts/Solana/get_balance.js
@@ -1,11 +1,23 @@
 import { PublicKey, Connection, clusterApiUrl } from '@solana/web3.js';
 
+/**
+ * Retrieves the balance of a Solana wallet address.
+ * @param {string} publicKey - The public key of the wallet address.
+ * @returns {Promise<number>} - A promise that resolves to the balance of the wallet address.
+ */
 const get_balance = async (publicKey) => {
+  // Creating a new Connection instance using the clusterApiUrl for the testnet
   let connection = new Connection(clusterApiUrl("testnet"));
 
+  // Converting the provided publicKey string to a PublicKey object
   publicKey = new PublicKey(publicKey);
+
+  // Retrieving the balance of the wallet address from the Solana network
   let balance = await connection.getBalance(publicKey);
-  return balance
+
+  // Returning the balance
+  return balance;
 }
 
-export default get_balance
+// Exporting the get_balance function as the default export of the module
+export default get_balance;
